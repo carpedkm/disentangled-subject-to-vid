53f83a63e6143be2c59a3ed2bcfe56638ce989d4
carpedkm <flytodk@gmail.com>
2024-11-06 02:50:47 +0000

    changes

---
+++ finetune/finetune_multi_rank_videocustom_a100_40_48.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+NODES=$1
+export MODEL_PATH="THUDM/CogVideoX-5b"
+export CACHE_PATH="~/.cache"
+export DATASET_PATH="/mnt/carpedkm_data/preprocessed_subset4000/background_only_boxes"
+export ANNO_PATH="../annotation/video_dict_fullset400k.json"
+export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/finetune4000_multi_node_a100_40_48"
+export VALIDATION_REF_PATH="./val_samples/"
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_IB_DISABLE=0
+export NCCL_IB_PCI_RELAXED_ORDERING=1
+export NCCL_SOCKET_IFNAME=eth0
+export NCCL_NET_GDR_LEVEL=5
+export NCCL_TOPO_FILE=/opt/microsoft/ndv4-topo.xml
+export NCCL_TIMEOUT=600  # Increase the timeout to 600 seconds
+
+# if you are not using wth 8 gus, change `accelerate_config_machine_single.yaml` num_processes as your gpu number
+accelerate launch --config_file accelerate_config_machine_multi_48gpu.yaml --multi_gpu --machine_rank ${NODES} \
+  train_cogvideox_for_videocustom_inf_fix.py \
+  --gradient_checkpointing \
+  --pretrained_model_name_or_path $MODEL_PATH \
+  --cache_dir $CACHE_PATH \
+  --enable_tiling \
+  --enable_slicing \
+  --instance_data_root $DATASET_PATH \
+  --anno_root $ANNO_PATH \
+  --validation_epochs 2 \
+  --validation_reference_image $VALIDATION_REF_PATH \
+  --seed 42 \
+  --rank 128 \
+  --lora_alpha 64 \
+  --mixed_precision bf16 \
+  --output_dir $OUTPUT_PATH \
+  --height 480 \
+  --width 720 \
+  --fps 8 \
+  --max_num_frames 49 \
+  --skip_frames_start 0 \
+  --skip_frames_end 0 \
+  --train_batch_size 2 \
+  --num_train_epochs 30 \
+  --checkpointing_steps 200 \
+  --gradient_accumulation_steps 1 \
+  --learning_rate 5e-5 \
+  --lr_scheduler cosine_with_restarts \
+  --lr_warmup_steps 200 \
+  --lr_num_cycles 1 \
+  --enable_slicing \
+  --enable_tiling \
+  --gradient_checkpointing \
+  --optimizer AdamW \
+  --adam_beta1 0.9 \
+  --adam_beta2 0.95 \
+  --max_grad_norm 1.0 \
+  --allow_tf32 \
+  --report_to wandb
+  # --validation_prompt "DISNEY A black and white animated scene unfolds with an anthropomorphic goat surrounded by musical notes and symbols, suggesting a playful environment. Mickey Mouse appears, leaning forward in curiosity as the goat remains still. The goat then engages with Mickey, who bends down to converse or react. The dynamics shift as Mickey grabs the goat, potentially in surprise or playfulness, amidst a minimalistic background. The scene captures the evolving relationship between the two characters in a whimsical, animated setting, emphasizing their interactions and emotions:::A panda, dressed in a small, red jacket and a tiny hat, sits on a wooden stool in a serene bamboo forest. The panda's fluffy paws strum a miniature acoustic guitar, producing soft, melodic tunes. Nearby, a few other pandas gather, watching curiously and some clapping in rhythm. Sunlight filters through the tall bamboo, casting a gentle glow on the scene. The panda's face is expressive, showing concentration and joy as it plays. The background includes a small, flowing stream and vibrant green foliage, enhancing the peaceful and magical atmosphere of this unique musical performance" \
+  # --validation_prompt_separator ::: \
+  # --num_validation_videos 1 \
