935a3c91befc4faa8c04d4bc3e7ec653a1a65065
carpedkm <flytodk@gmail.com>
2024-11-19 12:55:22 +0000

    chnage

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu121
 
-cd videocustom
+# cd videocustom
 git checkout develop
 cd ..
 mv annotation videocustom/
