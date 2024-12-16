6aa630881d90c5d8e080f6d358bffbdc78afd18b
carpedkm <flytodk@gmail.com>
2024-12-16 02:47:40 +0000

    changes

--- finetune/refactored_a100_40_8_moviegen_wobg_crosspairs_144_2b.sh
+++ finetune/refactored_a100_40_8_moviegen_wobg_crosspairs_144_2b.sh
@@ -34,7 +34,7 @@ accelerate launch --config_file accelerate_config_machine_single_8gpu.yaml --mul
   --max_num_frames 49 \
   --skip_frames_start 0 \
   --skip_frames_end 0 \
-  --train_batch_size 6 \
+  --train_batch_size 4 \
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
