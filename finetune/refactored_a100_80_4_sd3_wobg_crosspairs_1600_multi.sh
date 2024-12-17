79dc86d50aaa3acbd70680d3b82f460a40c4bf8a
carpedkm <flytodk@gmail.com>
2024-12-17 07:41:01 +0000

    changes

--- finetune/refactored_a100_80_4_sd3_wobg_crosspairs_1600_multi.sh
+++ finetune/refactored_a100_80_4_sd3_wobg_crosspairs_1600_multi.sh
@@ -3,17 +3,16 @@
 export MODEL_PATH="THUDM/CogVideoX-5b"
 export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/preprocessed_4k_updated/background_only_boxes"
-export DATASET_PATH="/mnt/carpedkm_data/preprocessed_4k_updated/background_only_boxes"
 export ANNO_PATH="../annotation/video_dict_foreground_subset4000.json"
-export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/241217/finetune1600_concat_padded_wo_bg_cross_pairs"
+export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/241217/sd3_wo_bg_cross_pairs_scene_driven_1600"
 export VALIDATION_REF_PATH="./val_samples2/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 # if you are not using wth 8 gus, change `accelerate_config_machine_single.yaml` num_processes as your gpu number
-accelerate launch --config_file accelerate_config_machine_single_8gpu.yaml --multi_gpu \
-  train_cogvideox_for_videocustom_inf_fix.py \
+accelerate launch --config_file accelerate_config_machine_multi.yaml --multi_gpu  --machine_rank $1\
+  train_cogvideox_for_videocustom_refactored.py \
   --gradient_checkpointing \
   --pretrained_model_name_or_path $MODEL_PATH \
   --cache_dir $CACHE_PATH \
@@ -21,7 +20,7 @@ accelerate launch --config_file accelerate_config_machine_single_8gpu.yaml --mul
   --enable_slicing \
   --instance_data_root $DATASET_PATH \
   --anno_root $ANNO_PATH \
-  --validation_epochs 2 \
+  --validation_epochs 1 \
   --num_validation_videos 1 \
   --validation_reference_image $VALIDATION_REF_PATH \
   --seed 42 \
@@ -35,7 +34,7 @@ accelerate launch --config_file accelerate_config_machine_single_8gpu.yaml --mul
   --max_num_frames 49 \
   --skip_frames_start 0 \
   --skip_frames_end 0 \
-  --train_batch_size 2 \
+  --train_batch_size 6 \
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
