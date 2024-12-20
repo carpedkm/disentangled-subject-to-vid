c83cae33d9065f303c2502bbbc2bdbad3a29991a
carpedkm <flytodk@gmail.com>
2024-12-20 05:57:24 +0000

    deepspeed

--- finetune/refactored_a100_80_4_moviegen_1600_5b_w_latent.sh
+++ finetune/refactored_a100_80_4_moviegen_1600_5b_w_latent.sh
@@ -34,7 +34,7 @@ accelerate launch --config_file accelerate_config_machine_single_4gpu.yaml --mul
   --max_num_frames 49 \
   --skip_frames_start 0 \
   --skip_frames_end 0 \
-  --train_batch_size 2 \
+  --train_batch_size 8 \
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
