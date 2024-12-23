f0de7b0393548c66b91fc31eeb1e98336fc0757a
carpedkm <flytodk@gmail.com>
2024-12-23 00:58:27 +0000

    chnages

---
+++ finetune/auto_run_vae_160k.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Infinite loop to check GPU processes every 30 seconds
+while true; do
+    # Check if there are any active processes using the GPU
+    PROCESS_COUNT=$(nvidia-smi | grep -c "No running processes found")
+    
+    if [ "$PROCESS_COUNT" -gt 0 ]; then
+        echo "No GPU processes found. Running abc.sh..."
+        # Run abc.sh script
+        python vae_extractor_160k.py
+        echo "Script executed. Exiting..."
+        exit 0  # Exit the loop and script after running
+    else
+        echo "GPU is in use. Checking again in 30 seconds..."
+    fi
+
+    # Wait for 30 seconds before checking again
+    sleep 30
+done
