a2a9e26b40b5430039a0aaa4aef36c493311c327
carpedkm <flytodk@gmail.com>
2024-12-24 04:18:38 +0000

    changes

--- finetune/refactored_a100_80_4_controlnet_wobg_crosspairs_1600_w_latent_4fps_12.sh
+++ finetune/refactored_a100_80_4_controlnet_wobg_crosspairs_1600_w_latent_4fps_12.sh
@@ -7,11 +7,11 @@ export ANNO_PATH="../annotation/video_dict_foreground_subset4000.json"
 export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/241223/compare_controlnet_5b_w_latent_4000_xpairs_wobg_4fps_12frames"
 export VALIDATION_REF_PATH="./val_samples2/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 # if you are not using wth 8 gus, change `accelerate_config_machine_single.yaml` num_processes as your gpu number
-accelerate launch --config_file accelerate_config_machine_single_4gpu.yaml --multi_gpu \
+accelerate launch --config_file accelerate_config_machine_single_8gpu.yaml --multi_gpu \
   train_cogvideox_for_videocustom_wo_vae_241219.py \
   --gradient_checkpointing \
   --pretrained_model_name_or_path $MODEL_PATH \
@@ -34,7 +34,7 @@ accelerate launch --config_file accelerate_config_machine_single_4gpu.yaml --mul
   --max_num_frames 12 \
   --skip_frames_start 0 \
   --skip_frames_end 0 \
-  --train_batch_size 12 \
+  --train_batch_size 24 \
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
