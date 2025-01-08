612ac5ddd4cd5a4226ec91024e3af18a29abb74d
carpedkm <flytodk@gmail.com>
2025-01-08 04:06:36 +0000

    changes

--- finetune/train_cogvideox_for_videocustom_wo_vae_250106_image.py
+++ finetune/train_cogvideox_for_videocustom_wo_vae_250106_image.py
@@ -1887,7 +1887,7 @@ def main(args):
         # update random seed
         # set_seed(args.seed + epoch)
         for step, batch in enumerate(train_dataloader):
-            if epoch == first_epoch and step == 0:
+            if epoch == first_epoch and step == 0 and args.inference:
                 break
             # set_seed(args.seed + epoch)
             models_to_accumulate = [transformer]
