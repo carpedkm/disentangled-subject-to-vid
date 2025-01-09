39c2f15755ca7fbc722f70658bd2e2daf3adcb18
carpedkm <flytodk@gmail.com>
2025-01-09 07:28:50 +0000

    changes

--- finetune/refactored_a100_40_16_moviegen_50k_w_latent_single_frame_omini200_refined_prompt_vae_direct_inference.sh
+++ finetune/refactored_a100_40_16_moviegen_50k_w_latent_single_frame_omini200_refined_prompt_vae_direct_inference.sh
@@ -5,14 +5,14 @@ export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
 # export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/241223/compare_controlnet_5b_w_latent_4000_xpairs_wobg_single_frame"
-export OUTPUT_PATH="/mnt/carpedkm_data/result250109/result_check_200k_512x512_embedding_updated_prompt_with_prefix_dataloader_fixed"
+export OUTPUT_PATH="/mnt/carpedkm_data/result250109/result_check_100k_512x512_embedding_refined_prompt_without_subject_moviegenstyle_vaeadd"
 export VALIDATION_REF_PATH="./val_samples_im/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 # if you are not using wth 8 gus, change `accelerate_config_machine_single.yaml` num_processes as your gpu number
-accelerate launch --config_file accelerate_config_machine_single_4gpu.yaml --multi_gpu \
+accelerate launch --config_file accelerate_config_machine_single.yaml \
   train_cogvideox_for_videocustom_wo_vae_250106_image_vae.py \
   --gradient_checkpointing \
   --pretrained_model_name_or_path $MODEL_PATH \
@@ -35,7 +35,7 @@ accelerate launch --config_file accelerate_config_machine_single_4gpu.yaml --mul
   --max_num_frames 1 \
   --skip_frames_start 0 \
   --skip_frames_end 0 \
-  --train_batch_size 96 \
+  --train_batch_size 16 \
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
@@ -52,13 +52,13 @@ accelerate launch --config_file accelerate_config_machine_single_4gpu.yaml --mul
   --max_grad_norm 1.0 \
   --allow_tf32 \
   --t5_first \
-  --zero_conv_add \
-  --cross_pairs \
-  --wo_bg \
   --use_latent \
+  --vae_add \
   --load_to_ram \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
-  --report_to wandb 
+  --report_to wandb \
+  --inference \
+  --resume_from_checkpoint /mnt/carpedkm_data/result250109/result_check_100k_512x512_embedding_refined_prompt_without_subject_moviegenstyle_vaeadd/checkpoint-250
   # --validation_prompt "DISNEY A black and white animated scene unfolds with an anthropomorphic goat surrounded by musical notes and symbols, suggesting a playful environment. Mickey Mouse appears, leaning forward in curiosity as the goat remains still. The goat then engages with Mickey, who bends down to converse or react. The dynamics shift as Mickey grabs the goat, potentially in surprise or playfulness, amidst a minimalistic background. The scene captures the evolving relationship between the two characters in a whimsical, animated setting, emphasizing their interactions and emotions:::A panda, dressed in a small, red jacket and a tiny hat, sits on a wooden stool in a serene bamboo forest. The panda's fluffy paws strum a miniature acoustic guitar, producing soft, melodic tunes. Nearby, a few other pandas gather, watching curiously and some clapping in rhythm. Sunlight filters through the tall bamboo, casting a gentle glow on the scene. The panda's face is expressive, showing concentration and joy as it plays. The background includes a small, flowing stream and vibrant green foliage, enhancing the peaceful and magical atmosphere of this unique musical performance" \
   # --validation_prompt_separator ::: \
   # --num_validation_videos 1 \
