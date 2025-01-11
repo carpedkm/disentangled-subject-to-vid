67d762c0ab8e02d501abd787d9242cbd20dc9411
carpedkm <flytodk@gmail.com>
2025-01-11 07:50:13 +0000

    changes

--- vae_extract_for_omnicontrol_sub200k_p3.py
+++ vae_extract_for_omnicontrol_sub200k_p3.py
@@ -128,7 +128,7 @@ video_dir1 = "/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full/right_images_u
 video_paths = sorted([os.path.join(video_dir1, f) for f in os.listdir(video_dir1) if f.endswith(".png")]) # single frame video (image)
 total_cnt = len(video_paths)
 # half of the videos
-# video_paths = video_paths[:total_cnt//2]
+video_paths = video_paths[:total_cnt//2]
 print(f"Total video paths: {len(video_paths)}")
 # video_paths += [os.path.join(video_dir2, f) for f in os.listdir(video_dir2) if f.endswith(".png")] # single frame video (image)
 
@@ -136,8 +136,8 @@ print(f"Total video paths: {len(video_paths)}")
 extract_vae_latents(
     video_paths,
     vae_model_path="THUDM/CogVideoX-5b",
-    # output_dir="/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full/right_latents_updated",
-    output_dir = "/dev/shm/vae_latents",
+    output_dir="/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full/right_latents_updated",
+    # output_dir = "/dev/shm/vae_latents",
     height=480,
     width=720,
     max_frames=1,
