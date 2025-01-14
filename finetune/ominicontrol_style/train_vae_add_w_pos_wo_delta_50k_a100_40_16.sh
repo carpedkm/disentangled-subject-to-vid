88638d01acf69afd0bf59f300b7bec01dcc9bda6
carpedkm <flytodk@gmail.com>
2025-01-14 05:21:12 +0000

    change

--- finetune/ominicontrol_style/train_vae_add_w_pos_wo_delta_50k_a100_40_16.sh
+++ finetune/ominicontrol_style/train_vae_add_w_pos_wo_delta_50k_a100_40_16.sh
@@ -5,7 +5,7 @@ export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
 # export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/241223/compare_controlnet_5b_w_latent_4000_xpairs_wobg_single_frame"
-export OUTPUT_PATH="/mnt/carpedkm_data/result25013/512x512embedding_refined_oministyle_vaeadd_w_posembed_200k_w_embed_lora"
+export OUTPUT_PATH="/mnt/carpedkm_data/result250114/512x512embedding_refined_oministyle_vaeadd_w_posembed_wo_pos_delta_50k_w_embed_lora"
 export VALIDATION_REF_PATH="../val_samples_im/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
@@ -58,7 +58,7 @@ accelerate launch --config_file ../accelerate_config_machine_multi.yaml --multi_
   --load_to_ram \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
   --report_to wandb \
-  --subset_cnt -1 \
+  --subset_cnt 5000 \
   --seen_validation
   # --validation_prompt "DISNEY A black and white animated scene unfolds with an anthropomorphic goat surrounded by musical notes and symbols, suggesting a playful environment. Mickey Mouse appears, leaning forward in curiosity as the goat remains still. The goat then engages with Mickey, who bends down to converse or react. The dynamics shift as Mickey grabs the goat, potentially in surprise or playfulness, amidst a minimalistic background. The scene captures the evolving relationship between the two characters in a whimsical, animated setting, emphasizing their interactions and emotions:::A panda, dressed in a small, red jacket and a tiny hat, sits on a wooden stool in a serene bamboo forest. The panda's fluffy paws strum a miniature acoustic guitar, producing soft, melodic tunes. Nearby, a few other pandas gather, watching curiously and some clapping in rhythm. Sunlight filters through the tall bamboo, casting a gentle glow on the scene. The panda's face is expressive, showing concentration and joy as it plays. The background includes a small, flowing stream and vibrant green foliage, enhancing the peaceful and magical atmosphere of this unique musical performance" \
   # --validation_prompt_separator ::: \
