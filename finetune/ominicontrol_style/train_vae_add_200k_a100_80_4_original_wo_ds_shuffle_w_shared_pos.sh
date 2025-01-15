c292ab2a4fb05ca9a9a16622a8559d9b679843f4
carpedkm <flytodk@gmail.com>
2025-01-15 15:28:26 +0000

    changes

--- finetune/ominicontrol_style/train_vae_add_200k_a100_80_4_original_wo_ds_shuffle_w_shared_pos.sh
+++ finetune/ominicontrol_style/train_vae_add_200k_a100_80_4_original_wo_ds_shuffle_w_shared_pos.sh
@@ -5,7 +5,7 @@ export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
 # export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/241223/compare_controlnet_5b_w_latent_4000_xpairs_wobg_single_frame"
-export OUTPUT_PATH="/mnt/carpedkm_data/result250115/512x512embedding_refined_oministyle_vaeadd_original_wo_ds_shuffle"
+export OUTPUT_PATH="/mnt/carpedkm_data/result250115/512x512embedding_refined_oministyle_vaeadd_original_wo_ds_shuffle_w_pos_embed"
 export VALIDATION_REF_PATH="../val_samples_im/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_VISIBLE_DEVICES=0,1,2,3
@@ -56,7 +56,8 @@ accelerate launch --config_file ../accelerate_config_machine_single_4gpu.yaml --
   --vae_add \
   --load_to_ram \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
-  --report_to wandb 
+  --report_to wandb \
+  --pos_embed 
   # --subset_cnt 200000 \
   # --inference \
   # --resume_from_checkpoint /mnt/carpedkm_data/result25012/512x512embedding_refined_oministyle_vaeadd/checkpoint-6000
