ee4b3295f824259462e8f143623fde6f4b70c129
carpedkm <flytodk@gmail.com>
2025-01-20 03:35:01 +0000

    changes

--- finetune/ominicontrol_style/train_vae_add_200k_a100_40_16_original_channel_fix.sh
+++ finetune/ominicontrol_style/train_vae_add_200k_a100_40_16_original_channel_fix.sh
@@ -29,8 +29,8 @@ accelerate launch --config_file ../accelerate_config_machine_multi.yaml --multi_
   --lora_alpha 64 \
   --mixed_precision bf16 \
   --output_dir $OUTPUT_PATH \
-  --height 512 \
-  --width 512 \
+  --height 480 \
+  --width 720 \
   --fps 8 \
   --max_num_frames 1 \
   --skip_frames_start 0 \
