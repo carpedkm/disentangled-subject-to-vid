97f5af90f64901cbb7f59696415383b1372519a4
carpedkm <flytodk@gmail.com>
2025-01-27 12:20:36 +0000

    changes

--- finetune/ominicontrol_style/exp_0126_train_vae_add_200k_a100_40_16_original_channel_fix_add_special_inf.sh
+++ finetune/ominicontrol_style/exp_0126_train_vae_add_200k_a100_40_16_original_channel_fix_add_special_inf.sh
@@ -5,15 +5,15 @@ export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
 # export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/241223/compare_controlnet_5b_w_latent_4000_xpairs_wobg_single_frame"
-export OUTPUT_PATH="/mnt/carpedkm_data/result250120/720x480embedding_refined_oministyle_vaeadd_original_channel_fix"
+export OUTPUT_PATH="/mnt/carpedkm_data/result250126/720x480embedding_refined_oministyle_vaeadd_original_channel_fix_pos_embed_add_special_tk"
 export VALIDATION_REF_PATH="../val_samples_im/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 # if you are not using wth 8 gus, change `accelerate_config_machine_single.yaml` num_processes as your gpu number
 accelerate launch --config_file ../accelerate_config_machine_single.yaml --multi_gpu \
-  ../train_cogvideox_for_videocustom_wo_vae_250106_image_vae_like_ominicontrol.py \
+  ../train_cogvideox_for_videocustom_wo_vae_250122_image_vae_like_ominicontrol_with_cross_attend.py \
   --gradient_checkpointing \
   --pretrained_model_name_or_path $MODEL_PATH \
   --cache_dir $CACHE_PATH \
@@ -39,7 +39,7 @@ accelerate launch --config_file ../accelerate_config_machine_single.yaml --multi
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
-  --learning_rate 1e-5 \
+  --learning_rate 5e-5 \
   --lr_scheduler cosine_with_restarts \
   --lr_warmup_steps 200 \
   --lr_num_cycles 1 \
@@ -54,11 +54,17 @@ accelerate launch --config_file ../accelerate_config_machine_single.yaml --multi
   --t5_first \
   --use_latent \
   --vae_add \
+  --pos_embed \
+  --add_special \
   --load_to_ram \
-  --inference \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
   --report_to wandb \
-  --resume_from_checkpoint /mnt/carpedkm_data/result250120/720x480embedding_refined_oministyle_vaeadd_original_channel_fix/checkpoint-20000 \
+  --inference \
+  --seen_validation \
+  --resume_from_checkpoint checkpoint-8400 \
+  # --seen_validation
+  # --resume_from_checkpoint /mnt/carpedkm_data/result250120/720x480embedding_refined_oministyle_vaeadd_original_channel_fix/checkpoint-500
+  # --subset_cnt 200000 \
   # --inference \
   # --resume_from_checkpoint /mnt/carpedkm_data/result25012/512x512embedding_refined_oministyle_vaeadd/checkpoint-6000
   # --validation_prompt "DISNEY A black and white animated scene unfolds with an anthropomorphic goat surrounded by musical notes and symbols, suggesting a playful environment. Mickey Mouse appears, leaning forward in curiosity as the goat remains still. The goat then engages with Mickey, who bends down to converse or react. The dynamics shift as Mickey grabs the goat, potentially in surprise or playfulness, amidst a minimalistic background. The scene captures the evolving relationship between the two characters in a whimsical, animated setting, emphasizing their interactions and emotions:::A panda, dressed in a small, red jacket and a tiny hat, sits on a wooden stool in a serene bamboo forest. The panda's fluffy paws strum a miniature acoustic guitar, producing soft, melodic tunes. Nearby, a few other pandas gather, watching curiously and some clapping in rhythm. Sunlight filters through the tall bamboo, casting a gentle glow on the scene. The panda's face is expressive, showing concentration and joy as it plays. The background includes a small, flowing stream and vibrant green foliage, enhancing the peaceful and magical atmosphere of this unique musical performance" \
