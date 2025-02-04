ef28715a3f4df5d2fa0e726ba16ca657ee0c5d56
carpedkm <flytodk@gmail.com>
2025-02-04 11:54:07 +0000

    update check attention map

--- finetune/ominicontrol_style/exp_0126_train_vae_add_200k_a100_40_16_original_channel_fix_add_special_inf.sh
+++ finetune/ominicontrol_style/exp_0126_train_vae_add_200k_a100_40_16_original_channel_fix_add_special_inf.sh
@@ -60,7 +60,7 @@ accelerate launch --config_file ../accelerate_config_machine_single.yaml --multi
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
   --report_to wandb \
   --inference \
-  --resume_from_checkpoint checkpoint-6600 \
+  --resume_from_checkpoint checkpoint-30000 \
   # --seen_validation
   # --resume_from_checkpoint /mnt/carpedkm_data/result250120/720x480embedding_refined_oministyle_vaeadd_original_channel_fix/checkpoint-500
   # --subset_cnt 200000 \
