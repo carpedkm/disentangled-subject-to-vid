c0a8d61294deb669daad0d79de7662b9f4bd5aa8
carpedkm <flytodk@gmail.com>
2025-02-18 08:30:07 +0000

    changes

--- finetune/ominicontrol_style/exp_0218_train_vae_add_200k_a100_40_16_joint_finetune.sh
+++ finetune/ominicontrol_style/exp_0218_train_vae_add_200k_a100_40_16_joint_finetune.sh
@@ -43,7 +43,7 @@ accelerate launch --config_file ../accelerate_config_machine_multi.yaml --multi_
   --max_num_frames 49 \
   --skip_frames_start 0 \
   --skip_frames_end 0 \
-  --train_batch_size 1 \
+  --train_batch_size 8 \
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
