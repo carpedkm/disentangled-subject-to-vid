85dfc234b920ab270cea27e95b8d15a8624cb10b
carpedkm <flytodk@gmail.com>
2025-02-21 04:35:31 +0000

    changes

--- finetune/ominicontrol_style/exp_0221_train_vae_add_200k_a100_80_4_joint_finetune_random_full_drop_weighted_frame.sh
+++ finetune/ominicontrol_style/exp_0221_train_vae_add_200k_a100_80_4_joint_finetune_random_full_drop_weighted_frame.sh
@@ -3,25 +3,15 @@ export MODEL_PATH="THUDM/CogVideoX-5b"
 export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
-export OUTPUT_PATH="/mnt/carpedkm_data/result250220/joint_finetune_random_frame_select_zero_pad_prob01"
+export OUTPUT_PATH="/mnt/carpedkm_data/result250221/joint_finetune_no_randomdrop_weighted_frame_80_4"
 export VALIDATION_REF_PATH="../val_samples_im/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 
-export NCCL_IB_DISABLE=0
-export NCCL_IB_PCI_RELAXED_ORDERING=1
-export NCCL_SOCKET_IFNAME=eth0
-export NCCL_NET_GDR_LEVEL=5
-export NCCL_TOPO_FILE=/opt/microsoft/ndv4-topo.xml
-export NCCL_TIMEOUT=600  # Increase the timeout to 600 seconds
-
-RANDOM_PORT=$((49152 + RANDOM % 16384))
-
-accelerate launch --config_file ../accelerate_config_machine_multi.yaml --multi_gpu --machine_rank ${NODE_RANK} \
-  --main_process_port ${MASTER_PORT} \
-  ../train_0219_randomdrop.py \
+accelerate launch --config_file ../accelerate_config_machine_single_4gpu.yaml --multi_gpu \
+  ../train_0221_lossscale.py \
   --gradient_checkpointing \
   --pretrained_model_name_or_path $MODEL_PATH \
   --cache_dir $CACHE_PATH \
@@ -29,7 +19,7 @@ accelerate launch --config_file ../accelerate_config_machine_multi.yaml --multi_
   --enable_slicing \
   --instance_data_root $DATASET_PATH \
   --anno_root $ANNO_PATH \
-  --validation_epochs 100 \
+  --validation_epochs 1 \
   --num_validation_videos 1 \
   --validation_reference_image $VALIDATION_REF_PATH \
   --seed 42 \
@@ -43,7 +33,7 @@ accelerate launch --config_file ../accelerate_config_machine_multi.yaml --multi_
   --max_num_frames 49 \
   --skip_frames_start 0 \
   --skip_frames_end 0 \
-  --train_batch_size 8 \
+  --train_batch_size 32 \
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
@@ -67,14 +57,15 @@ accelerate launch --config_file ../accelerate_config_machine_multi.yaml --multi_
   --non_shared_pos_embed \
   --add_special \
   --layernorm_fix \
-  --joint_random_pad_zerotrain \
-  -- \
-  --prob_sample_video 0.1 \
+  --joint_train \
+  --random_drop_full \
+  --prob_sample_video 0.2 \
+  --random_drop_prob 0.0 \
   --video_anno /mnt/carpedkm_data/image_gen_ds/second_stage_video_train/second_stage_video_filtered_data_dict_sampled_4k.json \
   --video_instance_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels \
-  --video_ref_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_rand \
+  --video_ref_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_first \
   --load_to_ram \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
   --report_to wandb \
-  # --inference 
+  # --inference 채
   # --resume_from_checkpoint /mnt/carpedkm_data/result250215/special_tk_layernorm_fix_pos_embed_fix_40_16_non_shared_random_fix/checkpoint-3000 
