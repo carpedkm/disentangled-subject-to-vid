853b4c501e59618afc343d368f14cafad5ad2394
carpedkm <flytodk@gmail.com>
2025-02-21 12:26:46 +0000

    changes

--- finetune/ominicontrol_style/exp_0221_train_vae_add_200k_a100_40_16_joint_finetune_random_full_drop_0.9.sh
+++ finetune/ominicontrol_style/exp_0221_train_vae_add_200k_a100_40_16_joint_finetune_random_full_drop_0.9.sh
@@ -3,14 +3,24 @@ export MODEL_PATH="THUDM/CogVideoX-5b"
 export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
-export OUTPUT_PATH="/mnt/carpedkm_data/result250220/joint_finetune_randomdrop_full_40_16"
+export OUTPUT_PATH="/mnt/carpedkm_data/result250221/joint_finetune_randomdrop_full_40_16_dropprob_0.9"
 export VALIDATION_REF_PATH="../val_samples_im/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 
-accelerate launch --config_file ../accelerate_config_machine_single.yaml --multi_gpu \
+export NCCL_IB_DISABLE=0
+export NCCL_IB_PCI_RELAXED_ORDERING=1
+export NCCL_SOCKET_IFNAME=eth0
+export NCCL_NET_GDR_LEVEL=5
+export NCCL_TOPO_FILE=/opt/microsoft/ndv4-topo.xml
+export NCCL_TIMEOUT=600  # Increase the timeout to 600 seconds
+
+RANDOM_PORT=$((49152 + RANDOM % 16384))
+
+accelerate launch --config_file ../accelerate_config_machine_multi.yaml --multi_gpu --machine_rank ${NODE_RANK} \
+  --main_process_port ${MASTER_PORT} \
   ../train_0219_randomdrop.py \
   --gradient_checkpointing \
   --pretrained_model_name_or_path $MODEL_PATH \
@@ -59,6 +69,7 @@ accelerate launch --config_file ../accelerate_config_machine_single.yaml --multi
   --layernorm_fix \
   --joint_train \
   --random_drop_full \
+  --random_drop_prob 1.0 \
   --prob_sample_video 0.2 \
   --video_anno /mnt/carpedkm_data/image_gen_ds/second_stage_video_train/second_stage_video_filtered_data_dict_sampled_4k.json \
   --video_instance_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels \
@@ -66,7 +77,5 @@ accelerate launch --config_file ../accelerate_config_machine_single.yaml --multi
   --load_to_ram \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
   --report_to wandb \
-  --inference \
-  --resume_from_checkpoint checkpoint-9200
   # --inference 채
   # --resume_from_checkpoint /mnt/carpedkm_data/result250215/special_tk_layernorm_fix_pos_embed_fix_40_16_non_shared_random_fix/checkpoint-3000 
