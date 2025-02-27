c018b39b8f008837735a7791407c057dd2d2e590
carpedkm <flytodk@gmail.com>
2025-02-27 06:45:01 +0000

    changes

--- finetune/ominicontrol_style/exp_0227_train_vae_add_200k_a100_40_32_palisades_joint_finetune_random_multi_i2v_random_drop_full_0.5_vid_ratio_0.4.sh
+++ finetune/ominicontrol_style/exp_0227_train_vae_add_200k_a100_40_32_palisades_joint_finetune_random_multi_i2v_random_drop_full_0.5_vid_ratio_0.4.sh
@@ -3,15 +3,24 @@ export MODEL_PATH="THUDM/CogVideoX-5b"
 export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
-export OUTPUT_PATH="/mnt/carpedkm_data/result250225/joint_finetune_random_drop_full_05_first_frame_for_i2v_with_pexels_10k_40G16"
-export VALIDATION_REF_PATH="../dreambooth_test_white/"
+export OUTPUT_PATH="/mnt/carpedkm_data/result250227/joint_finetune_random_frame_select_8fps_prob04_dropfull_prob05_palisades_40G32"
+export VALIDATION_REF_PATH="../val_samples_im/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 
+export NCCL_IB_DISABLE=0
+export NCCL_IB_PCI_RELAXED_ORDERING=1
+export NCCL_SOCKET_IFNAME=eth0
+export NCCL_NET_GDR_LEVEL=5
+export NCCL_TOPO_FILE=/opt/microsoft/ndv4-topo.xml
+export NCCL_TIMEOUT=600  # Increase the timeout to 600 seconds
 
-accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
+RANDOM_PORT=$((49152 + RANDOM % 16384))
+
+accelerate launch --config_file ../accelerate_config_machine_multi_4nodes.yaml --multi_gpu --machine_rank ${NODE_RANK} \
+  --main_process_port ${MASTER_PORT} \
   ../train_0219_randomdrop.py \
   --gradient_checkpointing \
   --pretrained_model_name_or_path $MODEL_PATH \
@@ -59,16 +68,13 @@ accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
   --add_special \
   --layernorm_fix \
   --joint_train \
-  --prob_sample_video 0.2 \
+  --prob_sample_video 0.4 \
   --random_drop_full \
-  --video_anno /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_10k/second_stage_video_filtered_data_dict_sampled_10k.json \
-  --video_instance_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_8fps_10k \
-  --video_ref_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_8fps_10k_first \
+  --video_anno /mnt/carpedkm_data/image_gen_ds/second_stage_video_train/second_stage_video_filtered_data_dict_sampled_4k.json \
+  --video_instance_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_8fps \
+  --video_ref_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_8fps_rand_multi \
   --load_to_ram \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
   --report_to wandb \
-  --inference \
-  --resume_from_checkpoint checkpoint-10000 \
-  --phase_name test \
   # --inference 
-  # --resume_from_checkpoint /mnt/carpedkm_data/result250215/special_tk_lajyernorm_fix_pos_embed_fix_40_16_non_shared_random_fix/checkpoint-3000 
+  # --resume_from_checkpoint /mnt/carpedkm_data/result250215/special_tk_layernorm_fix_pos_embed_fix_40_16_non_shared_random_fix/checkpoint-3000 
