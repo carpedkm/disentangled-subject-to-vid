8bf1c217b7d7e3f059dc54e79056804a36400730
carpedkm <flytodk@gmail.com>
2025-03-04 16:25:05 +0000

    changes

--- motion_evaluation/evaluate_FVD/src/evaluation_scripts/evaluate_your_model.sh
+++ motion_evaluation/evaluate_FVD/src/evaluation_scripts/evaluate_your_model.sh
@@ -5,7 +5,8 @@ Path_to_Benchmark="/root/daneul/projects/refactored/CogVideo/Pexels_subset_100K_
 # Path_to_Synthesized_Video="/root/daneul/projects/refactored/CogVideo/Pexels_evaluation_100K_200_0225_original"
 # Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/I2V_baseline/Temporal_eval"
 # Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/original_0.2"
-Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/only_image_training_ckpt3000"
+# Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/only_image_training_ckpt3000"
+Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/two_stage_ckpt1k"
 Save_Path="I2V_baseline"
 
 # Loop through dataset sizes
