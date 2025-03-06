2d4c5914d2eb2cd054e33739dc8182e22fa27118
carpedkm <flytodk@gmail.com>
2025-03-06 09:58:19 +0000

    changes

--- finetune/ominicontrol_style/image_only_inf_temporal_evaluation_large_p2 copy.sh
+++ finetune/ominicontrol_style/image_only_inf_temporal_evaluation_large_p2 copy.sh
@@ -1,20 +1,22 @@
+#!/bin/bash
 
 export MODEL_PATH="THUDM/CogVideoX-5b"
 export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
-export OUTPUT_PATH="/mnt/carpedkm_data/result250225/two_stage_id_and_i2v/"
-
+# export OUTPUT_PATH="/mnt/carpedkm_data/finetune_result/241223/compare_controlnet_5b_w_latent_4000_xpairs_wobg_single_frame"
+export OUTPUT_PATH="/mnt/carpedkm_data/result250215/special_tk_layernorm_fix_pos_embed_fix_40_16_non_shared_random_fix"
 export VALIDATION_REF_PATH="../zs_samples"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=2
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 
 export TEMPORAL_EVAL_PROMPT_PATH="/mnt/carpedkm_data/image_gen_ds/Pexels_subset_100K_fps8_flow-25-50_sample500/large/metadata.jsonl"
 export TEMPORAL_EVAL_FIRST_FRAME="/mnt/carpedkm_data/image_gen_ds/Pexels_subset_100K_fps8_flow-25-50_sample500/large/first_frame"
-export TEMPORAL_EVAL_SAVE_DIR="/mnt/carpedkm_data/temporal_eval_result/two_stage_ckpt1k"
+export TEMPORAL_EVAL_SAVE_DIR="/mnt/carpedkm_data/temporal_eval_result/only_image_training_ckpt3000"
 
+# if you are not using wth 8 gus, change `accelerate_config_machine_single.yaml` num_processes as your gpu number
 accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
   ../train_0302_temporaleval.py \
   --gradient_checkpointing \
@@ -35,10 +37,10 @@ accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
   --height 480 \
   --width 720 \
   --fps 8 \
-  --max_num_frames 49 \
+  --max_num_frames 1 \
   --skip_frames_start 0 \
   --skip_frames_end 0 \
-  --train_batch_size 1 \
+  --train_batch_size 8 \
   --num_train_epochs 30 \
   --checkpointing_steps 50 \
   --gradient_accumulation_steps 1 \
@@ -60,17 +62,14 @@ accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
   --pos_embed \
   --pos_embed_inf_match \
   --non_shared_pos_embed \
+  --random_pos \
   --add_special \
   --layernorm_fix \
-  --video_anno /mnt/carpedkm_data/image_gen_ds/second_stage_video_train/second_stage_video_filtered_data_dict_sampled_4k.json \
-  --video_instance_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_8fps \
-  --video_ref_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_8fps_rand_multi \
   --load_to_ram \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
   --report_to wandb \
   --inference \
-  --resume_from_checkpoint checkpoint-1000 \
-  --phase_name test \
+  --resume_from_checkpoint checkpoint-3000 \
   --sampling_for_quali \
   --num_of_prompts 4 \
   --temporal_eval \
@@ -80,3 +79,10 @@ accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
   --temporal_eval_use_amount 200 \
   --temporal_eval_type large \
   --temporal_eval_shard 2
+  # --resume_from_checkpoint /mnt/carpedkm_data/result250120/720x480embedding_refined_oministyle_vaeadd_original_channel_fix/checkpoint-500
+  # --subset_cnt 200000 \
+  # --inference \
+  # --resume_from_checkpoint /mnt/carpedkm_data/result25012/512x512embedding_refined_oministyle_vaeadd/checkpoint-6000
+  # --validation_prompt "DISNEY A black and white animated scene unfolds with an anthropomorphic goat surrounded by musical notes and symbols, suggesting a playful environment. Mickey Mouse appears, leaning forward in curiosity as the goat remains still. The goat then engages with Mickey, who bends down to converse or react. The dynamics shift as Mickey grabs the goat, potentially in surprise or playfulness, amidst a minimalistic background. The scene captures the evolving relationship between the two characters in a whimsical, animated setting, emphasizing their interactions and emotions:::A panda, dressed in a small, red jacket and a tiny hat, sits on a wooden stool in a serene bamboo forest. The panda's fluffy paws strum a miniature acoustic guitar, producing soft, melodic tunes. Nearby, a few other pandas gather, watching curiously and some clapping in rhythm. Sunlight filters through the tall bamboo, casting a gentle glow on the scene. The panda's face is expressive, showing concentration and joy as it plays. The background includes a small, flowing stream and vibrant green foliage, enhancing the peaceful and magical atmosphere of this unique musical performance" \
+  # --validation_prompt_separator ::: \
+  # --num_validation_videos 1 \
