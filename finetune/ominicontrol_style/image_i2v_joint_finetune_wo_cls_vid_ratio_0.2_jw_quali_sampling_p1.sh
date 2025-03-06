49b476fbe18fb467ce3348fe866535e4d66928fd
carpedkm <flytodk@gmail.com>
2025-03-06 01:51:44 +0000

    changese

--- finetune/ominicontrol_style/image_i2v_joint_finetune_wo_cls_vid_ratio_0.2_jw_quali_sampling_p1.sh
+++ finetune/ominicontrol_style/image_i2v_joint_finetune_wo_cls_vid_ratio_0.2_jw_quali_sampling_p1.sh
@@ -4,7 +4,6 @@ export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/daneul/image_gen_ds/omini200k_720p_full"
 export ANNO_PATH="/mnt/carpedkm_data/daneul/image_gen_ds/omini200k_720p_full/metadata_omini200k_update_refined.json"
 export OUTPUT_PATH="/mnt/carpedkm_data/result250302/joint_finetune_random_frame_select_8fps_prob02_drop_prob05_wo_cls_40G16_jw"
-<<<<<<< HEAD
 export VALIDATION_REF_PATH="../zs_samples/"
 export TEST_PROMPT_PATH="../zs_prompts.json"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
@@ -12,15 +11,6 @@ export CUDA_VISIBLE_DEVICES=2
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 
-=======
-export VALIDATION_REF_PATH="../val_samples_im/"
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=0
-export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
-export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
-
-export TEST_PROMPT_PATH="../zs_prompts.json"
->>>>>>> 09aa221d4a6472a85f3f7d9a99d4cdf4c2716ad1
 
 accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
   ../train_0301_evalcodeupd.py \
