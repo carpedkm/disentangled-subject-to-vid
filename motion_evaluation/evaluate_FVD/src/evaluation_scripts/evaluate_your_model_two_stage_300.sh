81c083c1c8000571049643df46090cc4986c4993
carpedkm <flytodk@gmail.com>
2025-03-08 02:59:20 +0000

    changes

--- motion_evaluation/evaluate_FVD/src/evaluation_scripts/evaluate_your_model_two_stage_300.sh
+++ motion_evaluation/evaluate_FVD/src/evaluation_scripts/evaluate_your_model_two_stage_300.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Define dataset paths
-Path_to_Benchmark="/root/daneul/projects/refactored/CogVideo/Pexels_subset_100K_fps8_flow-25-50_sample500"
+Path_to_Benchmark="/mnt/carpedkm_data/image_gen_ds/Pexels_subset_100K_fps8_flow-25-50_sample500"
 # Path_to_Synthesized_Video="/root/daneul/projects/refactored/CogVideo/Pexels_evaluation_100K_200_0225_original"
 # Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/I2V_baseline/Temporal_eval"
 # Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/original_0.2"
-Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/t2v_vanilla_final"
+# Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/t2v_vanilla_final"
 # Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/only_image_training_ckpt3000"
-# Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/two_stage_ckpt1k"
-Save_Path="I2V_baseline"
+Path_to_Synthesized_Video="/mnt/carpedkm_data/temporal_eval_result/two_stage_ckpt1k"
+Save_Path="Two_stage_300"
 
 # Loop through dataset sizes
-for TARGET in small; do
+for TARGET in small medium large; do
     video_path_GT="${Path_to_Benchmark}/${TARGET}/video_frames"  # Path to real video dataset
     video_path_pred="${Path_to_Synthesized_Video}/${TARGET}/video_frames"  # Path to generated samples
     save_path="./Results/${Save_Path}/${TARGET}/"  # Path to save results
