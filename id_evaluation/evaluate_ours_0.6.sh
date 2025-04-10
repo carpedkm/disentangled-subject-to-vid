ef96c4fcf2c4912be743536b700685c682f5b3e2
carpedkm <flytodk@gmail.com>
2025-04-10 06:09:27 +0000

    changes"

---
+++ id_evaluation/evaluate_ours_0.6.sh
@@ -0,0 +1,17 @@
+
+export CUDA_VISIBLE_DEVICES=3
+#adaptively compute world_size from CUDA_VISIBLE_DEVICES
+export WORLD_SIZE=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
+
+
+torchrun --nproc_per_node $WORLD_SIZE --standalone evaluate.py \
+    --output_path /root/daneul/projects/refactored/CogVideo/id_evaluation/ours_0.6_output\
+    --video_dir /mnt/carpedkm_data/result250227/joint_finetune_random_frame_select_8fps_prob06_dropfull_prob05_jx_40G16/seed_42 \
+    --image_dir /root/daneul/projects/refactored/CogVideo/id_evaluation/processed_white_720x480 \
+    --json_path /root/daneul/projects/refactored/CogVideo/id_evaluation/ours_0.6_vid_to_img.json \
+    --prompt_json /root/daneul/projects/refactored/CogVideo/id_evaluation/ours_0.6_vid_to_prompt.json \
+    --mode 'default' \
+    --n_frames 16
+    # --regional_suffix_image regional_sam \
+
+    # --dimension
