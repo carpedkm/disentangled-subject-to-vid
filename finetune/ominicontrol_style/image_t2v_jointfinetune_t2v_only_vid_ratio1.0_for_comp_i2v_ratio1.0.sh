b607f60d34de8f56aefaef1f76be6e134ed18f3d
carpedkm <flytodk@gmail.com>
2025-04-21 12:37:31 +0000

    changes

--- finetune/ominicontrol_style/image_t2v_jointfinetune_t2v_only_vid_ratio1.0_for_comp_i2v_ratio1.0.sh
+++ finetune/ominicontrol_style/image_t2v_jointfinetune_t2v_only_vid_ratio1.0_for_comp_i2v_ratio1.0.sh
@@ -3,19 +3,16 @@ export MODEL_PATH="THUDM/CogVideoX-5b"
 export CACHE_PATH="~/.cache"
 export DATASET_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k_720p_full"
 export ANNO_PATH="/mnt/carpedkm_data/image_gen_ds/omini200k/metadata_omini200k_update_refined.json"
-export OUTPUT_PATH="/mnt/carpedkm_data/arxiv_ready_250410/t2v_pexels_finetune"
-export VALIDATION_REF_PATH="../zs_samples/"
+export OUTPUT_PATH="/mnt/carpedkm_data/arxiv_ready_250421/i2v_pexels_finetune"
+export VALIDATION_REF_PATH="../val_samples_im/"
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 export WANDB_API_KEY=b524799f98b5a09033fe24848862dcb2a68af571
 
-export TEMPORAL_EVAL_PROMPT_PATH="/mnt/carpedkm_data/image_gen_ds/Pexels_subset_100K_fps8_flow-25-50_sample500/medium/metadata.jsonl"
-export TEMPORAL_EVAL_FIRST_FRAME="/mnt/carpedkm_data/image_gen_ds/Pexels_subset_100K_fps8_flow-25-50_sample500/medium/first_frame"
-export TEMPORAL_EVAL_SAVE_DIR="/mnt/carpedkm_data/temporal_eval_result/vid_finetune_orig_comp_ckpt1000_fix_0419"
 
-accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
-  ../train_0302_temporaleval.py \
+accelerate launch --config_file ../accelerate_config_machine_single_4gpu.yaml --multi_gpu \
+  ../train_0219_randomdrop.py \
   --gradient_checkpointing \
   --pretrained_model_name_or_path $MODEL_PATH \
   --cache_dir $CACHE_PATH \
@@ -60,20 +57,16 @@ accelerate launch --config_file ../accelerate_config_machine_single_inf.yaml \
   --pos_embed_inf_match \
   --non_shared_pos_embed \
   --add_special \
+  --layernorm_fix \
+  --joint_train \
+  --prob_sample_video 1.0 \
+  --random_drop_full \
+  --random_drop_prob 0.0 \
+  --video_anno /mnt/carpedkm_data/image_gen_ds/second_stage_video_train/second_stage_video_filtered_data_dict_sampled_4k.json \
+  --video_instance_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_8fps \
+  --video_ref_root /mnt/carpedkm_data/image_gen_ds/second_stage_video_train_pexels_8fps_rand \
   --load_to_ram \
   --latent_data_root /mnt/carpedkm_data/pexels_4k_updatd_vae_latents\
   --report_to wandb \
-  --inference \
-  --resume_from_checkpoint checkpoint-4000 \
-  --sampling_for_quali \
-  --num_of_prompts 4 \
-  --temporal_eval \
-  --temporal_eval_prompt_path $TEMPORAL_EVAL_PROMPT_PATH \
-  --temporal_eval_first_frame $TEMPORAL_EVAL_FIRST_FRAME \
-  --temporal_eval_save_dir $TEMPORAL_EVAL_SAVE_DIR \
-  --temporal_eval_use_amount 300 \
-  --temporal_eval_type medium \
-  --temporal_eval_shard 3 \
-  --t2v_eval
   # --inference 
   # --resume_from_checkpoint /mnt/carpedkm_data/result250215/special_tk_layernorm_fix_pos_embed_fix_40_16_non_shared_random_fix/checkpoint-3000 
